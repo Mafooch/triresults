08b406df32906d9905e8f2bd4b18b598dd6d15d3
Mafooch <joshuamaffuccio@gmail.com>
2016-03-01 00:14:44 -0500

    Add customized calc_ave instance methods for LegResult subclasses

--- app/models/run_result.rb
+++ app/models/run_result.rb
@@ -1,10 +1,10 @@
 class RunResult < LegResult
   field :mmile, as: :minute_mile, type: Float
-  
+
   def calc_ave
     if event && secs
-      meters = event.meters
-      self.minute_mile = meters.nil? ? nil : 12.1
+      miles = event.miles
+      self.minute_mile = miles.nil? ? nil : (secs / 60) / miles
     end
   end
 end
