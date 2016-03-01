d71ebe09a061aa5a8ef210c9ed317d9a8d6fbfd7
Mafooch <joshuamaffuccio@gmail.com>
2016-03-01 16:10:35 -0500

    Implement 1:m relationship between Racer and Entrants

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,8 @@
 class Racer
   include Mongoid::Document
 
+  has_many :races, class_name: "Entrant", foreign_key: "racer.racer_id",
+    dependent: :nullify, order: :"race.date".desc
   embeds_one :info, as: :parent, class_name: "RacerInfo", autobuild: true
   # automatically create the object when the relationship is navigated
 
