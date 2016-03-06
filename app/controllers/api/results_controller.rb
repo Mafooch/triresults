543da7cf8e1a597b4bc566e28fdd6bd9b96967e1
Mafooch <joshuamaffuccio@gmail.com>
2016-03-06 18:52:19 -0500

    Add json partial for api/results

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -3,7 +3,6 @@ class Api::ResultsController < ApplicationController
     if !request.accept || request.accept == "*/*"
       render plain: "/api/races/#{params[:race_id]}/results"
     else
-
     end
   end
 
@@ -11,7 +10,8 @@ class Api::ResultsController < ApplicationController
     if !request.accept || request.accept == "*/*"
       render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
     else
-
+      @result = Race.find(params[:race_id]).entrants.where(id: params[:id]).first
+      render partial: "result", object: @result, status: :ok
     end
   end
 end
